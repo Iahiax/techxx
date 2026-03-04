6157a7d87b58ee7f347b766aadcf0c2ebcb4208b
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-04 05:20:06 +0300

    Created authentication controller for Nafath and Tawtheeq login

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+// authController.js
+
+const nafathLogin = (req, res) => {
+    // Logic for Nafath login
+};
+
+const tawtheeqLogin = (req, res) => {
+    // Logic for Tawtheeq login
+};
+
+module.exports = {
+    nafathLogin,
+    tawtheeqLogin,
+};
